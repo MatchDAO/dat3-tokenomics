53aa30460a3887eedaf58739e5bee257b3ca65dd
saibr <errorr0819@gmail.com>
2023-04-06 21:10:16 +0800

    mod:Optimized deployment scripts.

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
-DAT3='0xba6f578786f2ca745814d72b6240781680d85c1204f2253cbf5ad07ec3ee52a0'
-PROFILE="test1"
-BASE=`pwd `
+DAT3='0x4f48ef49472364f88267f14b49efe1a951ecb90819025c86955014a38c839075'
+PROFILE="test4"
 echo "dat3:' $DAT3'"
-echo "aptos move compile -->  $DAT3_PATH --bytecode-version 6 "
-echo `aptos move compile --save-metadata --package-dir  $DAT3_PATH --bytecode-version 6`
-echo""
-sleep 5
-echo "aptos move publish --> $DAT3_PATH --bytecode-version 6 "
-echo `aptos move publish --profile $PROFILE --assume-yes --package-dir  $DAT3_PATH --bytecode-version 6 `
-echo""
+BASE_PATH=`pwd `
+DAT3_COIN="$BASE_PATH/dat3-coin"
+DAT3_CORE="$BASE_PATH/dat3-core"
+DAT3_T_INTERFACE="$BASE_PATH/interface"
+DAT3_STAKING="$BASE_PATH/staking"
 
-sleep 3
-echo " dat3_pool::init_pool "
-echo `aptos move run --profile $PROFILE  --assume-yes --function-id $DAT3::dat3_pool::init_pool `
-echo""
-sleep 2
 echo " dat3_pool_routel::init"
-echo `aptos move run --profile $PROFILE  --assume-yes --function-id $DAT3::dat3_pool_routel::init`
+echo `aptos move run   --assume-yes --function-id $DAT3::dat3_pool_routel::init`
 echo""
-sleep 2
+sleep 3
+echo "aptos move publish --> $DAT3_COIN   "
+echo `aptos move publish --profile $PROFILE --assume-yes --package-dir  $DAT3_COIN  `
+cd ..
+DAT3_POOL="`pwd`/dat3-contract-core"
+echo "aptos move compile -->  $DAT3_POOL  "
+echo `aptos move compile --save-metadata --package-dir  $DAT3_POOL  `
+echo""
+sleep 3
+echo "aptos move publish --> $DAT3_POOL   "
+echo `aptos move publish --profile $PROFILE --assume-yes --package-dir  $DAT3_POOL  `
+echo""
+cd $BASE
+echo "aptos move compile -->  $DAT3_T_INTERFACE  "
+echo `aptos move compile --save-metadata --package-dir  $DAT3_T_INTERFACE  `
+echo""
+sleep 3
+echo "aptos move publish --> $DAT3_T_INTERFACE   "
+echo `aptos move publish --profile $PROFILE --assume-yes --package-dir  $DAT3_T_INTERFACE  `
+echo""
+echo "aptos move compile -->  $DAT3_STAKING  "
+echo `aptos move compile --save-metadata --package-dir  $DAT3_STAKING  `
+echo""
+sleep 3
+echo "aptos move publish --> $DAT3_STAKING   "
+echo `aptos move publish --profile $PROFILE --assume-yes --package-dir  $DAT3_STAKING  `
+echo""
+echo "aptos move compile -->  $DAT3_CORE  "
+echo `aptos move compile --save-metadata --package-dir  $DAT3_CORE  `
+echo""
+sleep 3
+echo "aptos move publish --> $DAT3_CORE   "
+echo `aptos move publish --profile $PROFILE --assume-yes --package-dir  $DAT3_CORE  `
+echo""
